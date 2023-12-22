d4e26ef3e13b81ff8c4ffdf8361beb600e36179b
everzet <ever.zet@gmail.com>
2023-12-22 01:16:51 +0000

    rework Store back into normal behaviour

--- apps/equinox/lib/equinox/store.ex
+++ apps/equinox/lib/equinox/store.ex
@@ -2,14 +2,15 @@ defmodule Equinox.Store do
   alias Equinox.Stream.StreamName
   alias Equinox.Events.{TimelineEvent, EventData}
 
+  @type t :: module()
   @type expected_version :: -1 | non_neg_integer()
   @type written_position :: non_neg_integer()
 
-  @type fetcher :: (StreamName.t() -> Enumerable.t(TimelineEvent.t()))
-  @type writer ::
-          (StreamName.t(), list(EventData.t()), expected_version() ->
-             {:ok, new_version :: written_position()}
-             | {:error, DuplicateMessageId.t() | StreamVersionConflict.t() | Exception.t()})
+  @callback fetch_events(StreamName.t(), from_version :: expected_version()) ::
+              Enumerable.t(TimelineEvent.t())
+  @callback write_events(StreamName.t(), list(EventData.t()), expected_version()) ::
+              {:ok, new_version :: written_position()}
+              | {:error, DuplicateMessageId.t() | StreamVersionConflict.t() | Exception.t()}
 
   defmodule DuplicateMessageId do
     defexception message: "Message with given ID already exists"
