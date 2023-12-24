1492689f7f396f728c59a8a451ddd9222441b1d8
everzet <ever.zet@gmail.com>
2023-12-24 17:43:54 +0000

    rework MessageDb.Store, allow configuring batch_size

--- apps/equinox_message_db/lib/store.ex
+++ apps/equinox_message_db/lib/store.ex
@@ -1,84 +1,60 @@
 defmodule Equinox.MessageDb.Store do
-  alias Equinox.Events.DomainEvent
-  alias Equinox.{State, Codec, Fold}
   alias Equinox.MessageDb.{Reader, Writer}
-
-  @callback load!(GenServer.server(), String.t(), State.t(), Codec.t(), Fold.t()) :: State.t()
-  @callback sync!(
-              GenServer.server(),
-              String.t(),
-              State.t(),
-              list(DomainEvent.t()),
-              Codec.ctx(),
-              Codec.t(),
-              Fold.t()
-            ) :: State.t()
-
-  defmodule Unoptimized do
-    @behaviour Equinox.MessageDb.Store
-    @batch_size 500
-
-    def load!(conn, stream_name, state, codec, fold) do
-      State.load!(state, codec, fold, fn ->
-        Reader.stream_stream_messages(conn, stream_name, state.version + 1, @batch_size)
-      end)
-    end
-
-    def sync!(conn, stream_name, state, events, ctx, codec, fold) do
-      State.sync!(state, events, ctx, codec, fold, fn event_data ->
-        case Writer.write_messages(conn, stream_name, event_data, state.version) do
-          {:ok, new_version} -> new_version
-          {:error, exception} -> raise exception
-        end
-      end)
-    end
-  end
-
-  defmodule LatestKnownEvent do
-    @behaviour Equinox.MessageDb.Store
-
-    def load!(conn, stream_name, state, codec, fold) do
-      State.load!(state, codec, fold, fn ->
-        case Reader.get_last_stream_message(conn, String.Chars.to_string(stream_name)) do
-          {:ok, message} -> [message]
-          {:error, exception} -> raise exception
-        end
-      end)
-    end
-
-    defdelegate sync!(conn, stream_name, state, events, ctx, codec, fold), to: Unoptimized
-  end
+  alias Equinox.State
 
   defmacro __using__(opts) do
-    w_conn = Keyword.fetch!(opts, :write_connection)
-    r_conn = Keyword.fetch!(opts, :read_connection)
+    fetch_conn = Keyword.fetch!(opts, :fetch_conn)
+    write_conn = Keyword.fetch!(opts, :write_conn)
+    batch_size = Keyword.get(opts, :batch_size, 500)
 
     quote do
       alias Equinox.MessageDb.Store
 
       defmodule Unoptimized do
         @behaviour Equinox.Store
+        @batch unquote(batch_size)
 
-        def sync!(stream, state, events, ctx, codec, fold) do
-          Store.Unoptimized.sync!(unquote(w_conn), stream, state, events, ctx, codec, fold)
-        end
+        def load!(stream, state, codec, fold),
+          do: Store.load_unoptimized!(unquote(fetch_conn), stream, state, codec, fold, @batch)
 
-        def load!(stream, state, codec, fold) do
-          Store.Unoptimized.load!(unquote(r_conn), stream, state, codec, fold)
-        end
+        def sync!(stream, state, events, ctx, codec, fold),
+          do: Store.sync!(unquote(write_conn), stream, state, events, ctx, codec, fold)
       end
 
       defmodule LatestKnownEvent do
         @behaviour Equinox.Store
 
-        def sync!(stream, state, events, ctx, codec, fold) do
-          Store.LatestKnownEvent.sync!(unquote(w_conn), stream, state, events, ctx, codec, fold)
-        end
+        def load!(stream, state, codec, fold),
+          do: Store.load_latest_known_event!(unquote(fetch_conn), stream, state, codec, fold)
 
-        def load!(stream, state, codec, fold) do
-          Store.LatestKnownEvent.load!(unquote(r_conn), stream, state, codec, fold)
-        end
+        def sync!(stream, state, events, ctx, codec, fold),
+          do: Store.sync!(unquote(write_conn), stream, state, events, ctx, codec, fold)
       end
     end
+  end
+
+  def sync!(conn, stream_name, state, events, ctx, codec, fold) do
+    State.sync!(state, events, ctx, codec, fold, fn event_data ->
+      case Writer.write_messages(conn, stream_name, event_data, state.version) do
+        {:ok, new_version} -> new_version
+        {:error, exception} -> raise exception
+      end
+    end)
+  end
+
+  def load_unoptimized!(conn, stream_name, state, codec, fold, batch_size) do
+    State.load!(state, codec, fold, fn ->
+      Reader.stream_stream_messages(conn, stream_name, state.version + 1, batch_size)
+    end)
+  end
+
+  def load_latest_known_event!(conn, stream_name, state, codec, fold) do
+    State.load!(state, codec, fold, fn ->
+      case Reader.get_last_stream_message(conn, String.Chars.to_string(stream_name)) do
+        {:ok, nil} -> []
+        {:ok, message} -> [message]
+        {:error, exception} -> raise exception
+      end
+    end)
   end
 end
--- apps/equinox_message_db/test/message_db/store_test.exs
+++ apps/equinox_message_db/test/message_db/store_test.exs
@@ -3,7 +3,7 @@ defmodule Equinox.MessageDb.StoreTest do
 
   alias Equinox.State
   alias Equinox.Events.EventData
-  alias Equinox.MessageDb.Store.{Unoptimized, LatestKnownEvent}
+  alias Equinox.MessageDb.Store
 
   defmodule NumberCodec do
     @behaviour Equinox.Codec
@@ -23,36 +23,82 @@ defmodule Equinox.MessageDb.StoreTest do
     def evolve(_, v), do: v
   end
 
-  describe "Unoptimized" do
-    test_in_isolation "syncs and loads state by writing and fetching all events", %{conn: conn} do
-      state = State.init(SumFold)
+  describe "sync!/7" do
+    @state State.init(SumFold)
 
+    test_in_isolation "syncs from initial state", %{conn: conn} do
+      assert %State{value: 9, version: 2} =
+               Store.sync!(conn, "num-1", @state, [2, 3, 4], :ctx, NumberCodec, SumFold)
+    end
+
+    test_in_isolation "syncs from intermediate state", %{conn: conn} do
       assert %State{value: 5, version: 1} =
-               Unoptimized.sync!(conn, "num-1", state, [2, 3], :ctx, NumberCodec, SumFold)
+               Store.sync!(conn, "num-1", @state, [2, 3], :ctx, NumberCodec, SumFold)
 
       assert %State{value: 9, version: 2} =
-               Unoptimized.sync!(conn, "num-1", State.new(5, 1), [4], :ctx, NumberCodec, SumFold)
-
-      assert %State{value: 9, version: 2} =
-               Unoptimized.load!(conn, "num-1", state, NumberCodec, SumFold)
-
-      assert %State{value: 9, version: 2} =
-               Unoptimized.load!(conn, "num-1", State.new(5, 1), NumberCodec, SumFold)
+               Store.sync!(conn, "num-1", State.new(5, 1), [4], :ctx, NumberCodec, SumFold)
     end
   end
 
-  describe "LatestKnownEvent" do
-    test_in_isolation "syncs and loads state by writing and fetching all events", %{conn: conn} do
-      state = State.init(SumFold)
+  describe "load_unoptimized!/6" do
+    @state State.init(SumFold)
+
+    test_in_isolation "loads from initial state", %{conn: conn} do
+      assert %State{} = Store.sync!(conn, "num-1", @state, [2, 3, 4], :ctx, NumberCodec, SumFold)
+
+      assert %State{value: 9, version: 2} =
+               Store.load_unoptimized!(conn, "num-1", @state, NumberCodec, SumFold, 100)
+    end
+
+    test_in_isolation "loads from intermediate state", %{conn: conn} do
+      assert %State{} = Store.sync!(conn, "num-1", @state, [2, 3, 4], :ctx, NumberCodec, SumFold)
+
+      assert %State{value: 9, version: 2} =
+               Store.load_unoptimized!(conn, "num-1", State.new(5, 1), NumberCodec, SumFold, 100)
+    end
+
+    test_in_isolation "handles empty streams", %{conn: conn} do
+      assert %State{value: 0, version: -1} =
+               Store.load_unoptimized!(conn, "num-1", @state, NumberCodec, SumFold, 100)
+    end
+
+    test_in_isolation "handles different batch sizes", %{conn: conn} do
+      assert %State{} = Store.sync!(conn, "num-1", @state, [2, 3, 4], :ctx, NumberCodec, SumFold)
+
+      assert %State{value: 9, version: 2} =
+               Store.load_unoptimized!(conn, "num-1", @state, NumberCodec, SumFold, 1)
+
+      assert %State{value: 9, version: 2} =
+               Store.load_unoptimized!(conn, "num-1", @state, NumberCodec, SumFold, 100)
+    end
+  end
+
+  describe "load_latest_known_event!/5" do
+    @state State.init(InsFold)
+
+    test_in_isolation "loads from initial state", %{conn: conn} do
+      assert %State{} = Store.sync!(conn, "num-1", @state, [2, 3, 4], :ctx, NumberCodec, InsFold)
 
       assert %State{value: 4, version: 2} =
-               LatestKnownEvent.sync!(conn, "num-1", state, [2, 3, 4], :ctx, NumberCodec, InsFold)
+               Store.load_latest_known_event!(conn, "num-1", @state, NumberCodec, InsFold)
+    end
+
+    test_in_isolation "loads from intermediate state", %{conn: conn} do
+      assert %State{} = Store.sync!(conn, "num-1", @state, [2, 3, 4], :ctx, NumberCodec, InsFold)
 
       assert %State{value: 4, version: 2} =
-               LatestKnownEvent.load!(conn, "num-1", state, NumberCodec, InsFold)
+               Store.load_latest_known_event!(
+                 conn,
+                 "num-1",
+                 State.new(3, 1),
+                 NumberCodec,
+                 InsFold
+               )
+    end
 
-      assert %State{value: 4, version: 2} =
-               LatestKnownEvent.load!(conn, "num-1", State.new(3, 1), NumberCodec, InsFold)
+    test_in_isolation "handles empty streams", %{conn: conn} do
+      assert %State{value: nil, version: -1} =
+               Store.load_latest_known_event!(conn, "num-1", @state, NumberCodec, SumFold)
     end
   end
 end
